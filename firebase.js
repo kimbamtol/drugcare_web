c76ec64523bb9ba48b6f12f602f0de848009294c
kimbamtol0304 <kimbamtol0304@gmail.com>
2024-05-24 18:19:30 +0900

    m

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
 const admin = require('firebase-admin');
+require('dotenv').config(); // 환경 변수를 로드
 
-// 환경 변수에서 서비스 계정 JSON을 파싱
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Firebase Admin SDK 초기화
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
