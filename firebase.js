e6df9c651282436d3db241a30bb9852ee5da136a
kimbamtol0304 <kimbamtol0304@gmail.com>
2024-05-24 22:03:55 +0900

    m

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
-// DRUGCARE_WEB/firebase.js
 const admin = require('firebase-admin');
-require('dotenv').config(); // 환경 변수를 로드합니다.
+require('dotenv').config(); // 환경 변수를 로드
 
-let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// base64로 인코딩된 서비스 계정 키 디코딩
+const serviceAccountBuffer = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64');
+const serviceAccount = JSON.parse(serviceAccountBuffer.toString('utf-8'));
 
 // `\\n`을 `\n`으로 변환
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
