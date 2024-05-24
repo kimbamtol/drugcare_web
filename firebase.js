a7a4d0d80112fd8c30436a1de2c8d7939801596f
kimbamtol0304 <kimbamtol0304@gmail.com>
2024-05-24 18:58:16 +0900

    m

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin');
 require('dotenv').config(); // 환경 변수를 로드합니다.
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Buffer.from을 사용하여 private_key를 변환
-serviceAccount.private_key = Buffer.from(serviceAccount.private_key, 'utf8').toString();
+// `\\n`을 `\n`으로 변환
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
